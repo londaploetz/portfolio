74dea3818d7b6a4a3fe48497df2d8647bfcad90c
londaploetz <lploetz@hotmail.com>
2023-01-31 17:56:05 -0600

    added new route and page design

---
+++ src/components/EachProject/imports.js
@@ -0,0 +1,13 @@
+import projectImg01 from '../../Assets/projectImg01.jpg';
+import projectImg02 from '../../Assets/projectImg02.jpg';
+// import projectImg03 from '../../Assets/projectImg03.png';
+// import projectImg04 from '../../Assets/projectImg04.png';
+// import projectImg05 from '../../Assets/projectImg05.png';
+
+export {
+    projectImg01,
+    projectImg02,
+    //   projectImg03,
+    //   projectImg04,
+    //   projectImg05,
+};
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Brand } from './Brand/Brand'; 
 export { default as SideNav } from './SideNav/SideNav';
 export { default as Home } from './Pages/Home';
-export { default as Footer } from './Footer/Footer';
+export { default as Project } from './Pages/Projects';
+export { default as Portfolio } from './Portfolio/Portfolio';
