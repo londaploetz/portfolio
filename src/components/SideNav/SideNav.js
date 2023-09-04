925530dbe131b4a2a05c0294826c16dee54d6674
londaploetz <lploetz@hotmail.com>
2023-09-04 16:40:53 -0500

    changed side nav location

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -2,7 +2,10 @@ import React from 'react';
 import './sidenav.css';
 import { Link, useLocation } from "react-router-dom";
 import HomeNav from '../Portfolio/HomeNav';
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const SideNav = (props) => {
 
@@ -14,43 +17,44 @@ const SideNav = (props) => {
   } else {
     return (
 
-      <div className='side-nav-bar'>
-        <h1 className='londa'>
-          londa <br />
-          ploetz
-        </h1>
-        <div className='circle-links'>
-          <a href='mailto:lploetz@hotmail.com'
-            target="_blank"
-            className="email-img"
-            rel="noopener noreferrer"
-          ></a>
-          <a href="https://github.com/londaploetz"
-            target="_blank"
-            className="github-img"
-            rel="noopener noreferrer"
-          ></a>
-          <a href="https://linkedin.com/in/londa-ploetz"
-            target="_blank"
-            className="linkedin-img"
-            rel="noopener noreferrer"
-          ></a>
-        </div>
-
-
-        <div className="connect-links">
-          <button className="btn-links" onClick={props.clickAboutMe}>about me</button>
-          <button className="btn-links">
+      <Container fluid className='navbar-container'>
+        <Row className="navbar-row">
+          <Col lg={6} sm={12}> </Col>
+          <Col lg={2} sm={12} >  <button className="btn-links" onClick={props.clickAboutMe}>about me</button> </Col>
+          <Col lg={2} sm={12}>  <button className="btn-links">
             <Link
               to="/projects"
               id="projects"
-              className='btn-links-projects'>
+              className='btn-links'>
               projects
             </Link></button>
-          <button className="btn-links" onClick={props.clickResume}>resume</button>
+          </Col>
+          <Col lg={2} sm={12} >
+            <button className="btn-links" onClick={props.clickResume}>resume</button>
+          </Col>
+  
+          {/* <Col lg={1} sm={12}>
+            <a href='mailto:lploetz@hotmail.com'
+              target="_blank"
+              className="email-img"
+              rel="noopener noreferrer"
+            ></a> </Col>
+          <Col lg={1} sm={12}>
+            <a href="https://github.com/londaploetz"
+              target="_blank"
+              className="github-img"
+              rel="noopener noreferrer"
+            ></a> </Col>
+          <Col lg={1} sm={12}>
+            <a href="https://linkedin.com/in/londa-ploetz"
+              target="_blank"
+              className="linkedin-img"
+              rel="noopener noreferrer"
+            ></a> </Col> */}
+        </Row>
+      </Container>
 
-        </div>
-      </div>
+
     )
   }
 };
